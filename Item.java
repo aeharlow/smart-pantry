18c4870ed66df9923b93330600bd11e934309ea2
aharlow51 <93774412+aeharlow@users.noreply.github.com>
2021-11-15 21:52:48 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -2,30 +2,41 @@ import java.util.*;
 
 abstract class Item{
     private String name;
+    private String type;
     private int shelf_life;
     private int quantity;
     private Date exp_date;
     private Date purch_date;
 
-    public Item(String n, int sl, int q){
+    public Item(String n, String t, int sl, int q){
         name = n;
+        type = t;
         shelf_life = sl;
         quantity = q;
         exp_date = calcExp(sl);
-        // grab current date for purch_date
+        purch_date = new Date(); // according to stackOverflow this initalizes with current date
     }
 
     private Date calcExp(int shelf_life){
-        // uhhhh
+        // we add the shelf life to the purch_date to get expected exp date
         return null;
     }
 
     // getters
     public String getName(){ return name; }
+    public String getType(){ return type; }
     public int getShelfLife(){ return shelf_life; }
     public int getQuantity(){ return quantity; }
     public Date getExpDate(){ return exp_date; }
     public Date getPurchDate(){ return purch_date; } 
+
+    // setters
+    public void setName(String n){ name = n; }
+    public void setType(String c){ type = c; }
+    public void setShelfLife(int sl){ shelf_life = sl; }
+    public void setQuanlity(int q){ quantity = q; }
+    public void setExpDate(Date d){ exp_date = d; }
+    public void setPurchDate(Date d){ purch_date = d; }
 
     public void remind(){
         return;
