be9cbf8609d7d59d2eaf7a637c1334fe1d084081
aharlow51 <93774412+aeharlow@users.noreply.github.com>
2021-11-21 14:04:22 -0500

    Update SmartPantry.java

---
+++ SmartPantry.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class SmartPantry {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        String test = new String();
+        test = scan.nextLine();
+
+        System.out.println(test);
+    }
+    
+}
