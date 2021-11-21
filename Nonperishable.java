2c5bf2807cd02b8becb4b28117be03229370f1fd
aharlow51 <93774412+aeharlow@users.noreply.github.com>
2021-11-21 16:36:09 -0500

    Update Nonperishable.java

--- Nonperishable.java
+++ Nonperishable.java
@@ -3,12 +3,4 @@ class Nonperishable extends Item{
     public Nonperishable(String n, int sl, int q){
         super(n, sl, q);
     }
-
-    /***************************************************
-     * do we really need all these classes?
-     * Why cant we just add a string attribute 
-     * that signals the type of item ansd have 
-     * perishable and non-perishable be regular classes?
-     ***************************************************/
-
 }
