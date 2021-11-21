f49889d7afd24e78da69396b92f7ae533ae370ec
xzzjoe <xzzjoe@bu.edu>
2021-11-21 16:47:01 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,8 +1,6 @@
 public class test {
     public static void main(String[] args) {
        System.out.println("Ashley's test");
-        System.out.println("Zhizhou's test");
-
        System.out.println("Ashleys secind test");
     }
 }
