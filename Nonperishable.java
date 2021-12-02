bff6616896bf0f966a550a8e8425f9126eefd297
aharlow51 <93774412+aeharlow@users.noreply.github.com>
2021-12-02 15:09:24 -0500

    Update Nonperishable.java

--- Nonperishable.java
+++ Nonperishable.java
@@ -2,33 +2,26 @@ import java.util.*;
 
 class Nonperishable implements Item{
     String name;
-    int shelf_life;
     int quantity;
     Calendar exp_date;
-    Calendar purch_date;
 
-    public Nonperishable(String n, int sl, int q){
+    public Nonperishable(String n, int q){
         name = n;
-        shelf_life = sl;
         quantity = q;
-        exp_date = calcExp(sl);
+        exp_date = calcExp();
     }
 
     // getters
     public String getName(){ return name; }
-    public int getShelfLife(){ return shelf_life; }
     public int getQuantity(){ return quantity; }
     public Calendar getExpDate(){ return exp_date; }
-    public Calendar getPurchDate(){ return purch_date; } 
     
     // setters
     public void setName(String n){ name = n; }
-    public void setShelfLife(int sl){ shelf_life = sl; }
     public void setQuanlity(int q){ quantity = q; }
-    public void setExpDate(Calendar d){ exp_date = d; }
-    public void setPurchDate(Calendar d){ purch_date = d; }
+    public void setExpire(Calendar d){ exp_date = d; }
 
-    public Calendar calcExp(int sl){
+    public Calendar calcExp(){
         Calendar ret = Calendar.getInstance();
         return ret;
     }
@@ -42,7 +35,9 @@ class Nonperishable implements Item{
         //ret = name + " - quantity: " + quantity + ", experation date: " + exp_date.toString();
 
         String temp_ret;
-        temp_ret = name + " - quantity: " + quantity + ", shelf life: " + shelf_life + " days";
+        temp_ret = name + " - quantity: " + quantity;
+
+        // add exp date
 
         return temp_ret;
     }
