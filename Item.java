f3b50ca44b9a2f07784babb843196bb0a1683b3a
aharlow51 <93774412+aeharlow@users.noreply.github.com>
2021-12-02 15:09:22 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -7,16 +7,15 @@ import java.util.*;
 interface Item{
     // getters
     public String getName();
-    public int getShelfLife();
     public int getQuantity();
     public Calendar getExpDate();
 
     // setters
     public void setName(String n);
-    public void setShelfLife(int sl);
     public void setQuanlity(int q);
+    public void setExpire(Calendar e);
 
-    public Calendar calcExp(int shelf_life);
+    public Calendar calcExp();
     public void edit();
     public String toString();
 }
