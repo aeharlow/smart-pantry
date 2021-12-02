8c76a68b45863fa2d13906a0434c6aa45ee82e95
aharlow51 <93774412+aeharlow@users.noreply.github.com>
2021-12-02 15:09:47 -0500

    Update Perishable.java

--- Perishable.java
+++ Perishable.java
@@ -2,37 +2,30 @@ import java.util.*;
 
 class Perishable implements Item{
     String name;
-    int shelf_life;
     int quantity;
     Calendar exp_date;
-    Calendar purch_date;
     boolean is_frozen;
     
-    public Perishable(String n, int sl, int q, boolean f){
+    public Perishable(String n, int q, boolean f){
         name = n;
-        shelf_life = sl;
         quantity = q;
-        exp_date = calcExp(sl);
+        exp_date = calcExp();
         is_frozen = f;
     }
 
     // getters
     public String getName(){ return name; }
-    public int getShelfLife(){ return shelf_life; }
     public int getQuantity(){ return quantity; }
     public Calendar getExpDate(){ return exp_date; }
-    public Calendar getPurchDate(){ return purch_date; } 
     public boolean getIsFrozen(){ return is_frozen; }
 
     // setters
     public void setName(String n){ name = n; }
-    public void setShelfLife(int sl){ shelf_life = sl; }
     public void setQuanlity(int q){ quantity = q; }
-    public void setExpDate(Calendar d){ exp_date = d; }
-    public void setPurchDate(Calendar d){ purch_date = d; }
+    public void setExpire(Calendar d){ exp_date = d; }
     public void getIsFrozen(boolean f){ is_frozen = f;}
 
-    public Calendar calcExp(int sl){
+    public Calendar calcExp(){
         Calendar ret = Calendar.getInstance();
         return ret;
     }
@@ -53,8 +46,9 @@ class Perishable implements Item{
         //     getExpDate().toString() + ", frozen: " + f;
 
         String temp_ret = new String();;
-        temp_ret = getName() + " - quantity: " + getQuantity() + ", shelf life: " + 
-            getShelfLife() + "days, frozen: " + f;
+        temp_ret = getName() + " - quantity: " + getQuantity() + ", frozen: " + f;
+
+        //add exp date
 
         return temp_ret;
     }
