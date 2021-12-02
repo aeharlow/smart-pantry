67d7a7afc436cae37aae6c24b62e974779332d75
aharlow51 <93774412+aeharlow@users.noreply.github.com>
2021-12-02 18:29:27 -0500

    Update Nonperishable.java

--- Nonperishable.java
+++ Nonperishable.java
@@ -3,23 +3,23 @@ import java.util.*;
 class Nonperishable implements Item{
     private String name;
     private int quantity;
-    private Calendar exp_date;
+    private Calendar expDate;
 
     public Nonperishable(String n, int q){
         name = n;
         quantity = q;
-        exp_date = calcExp();
+        expDate = calcExp();
     }
 
     // getters
     public String getName(){ return name; }
     public int getQuantity(){ return quantity; }
-    public Calendar getExpDate(){ return exp_date; }
+    public Calendar getExpDate(){ return expDate; }
     
     // setters
     public void setName(String n){ name = n; }
     public void setQuantity(int q){ quantity = q; }
-    public void setExpire(Calendar d){ exp_date = d; }
+    public void setExpire(Calendar d){ expDate = d; }
 
     /**
      * calcExpire(item) - calculates the expiration of nonperishable items 
@@ -36,18 +36,32 @@ class Nonperishable implements Item{
     }
 
     public String toDBString(){
-        return null;
+        String exp = calToStr(expDate);
+        String ret = name + "," + quantity + "," + exp;
+        return ret;
+    }
+
+    /**
+     * toString(c) - takes in a calender object and creates a string of the
+     * format "MM/DD/YYYY" with the same date for the sake of
+     * writing the date to the pantry files
+     */
+    private static String calToStr(Calendar c) {
+        String date;
+        int month, day, year;
+
+        month = c.get(Calendar.MONTH) + 1;
+        day = c.get(Calendar.DAY_OF_MONTH);
+        year = c.get(Calendar.YEAR);
+
+        date = month + "/" + day + "/" + year;
+
+        return date;
     }
 
     public String toString(){
-        //String ret;
-        //ret = name + " - quantity: " + quantity + ", experation date: " + exp_date.toString();
-
-        String temp_ret;
-        temp_ret = name + " - quantity: " + quantity;
-
-        // add exp date
-
-        return temp_ret;
+        String ret;
+        ret = name + " - quantity: " + quantity + ", experation date: " + calToStr(expDate);
+        return ret;
     }
 }
